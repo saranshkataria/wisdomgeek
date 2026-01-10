c45f18e9cca4ab62883cba8781580c06f70ce218
saranshkataria <saransh@wisdomgeek.com>
2026-01-10 07:17:59 -0800

    SEO updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Wisdom Geek";
+export const SITE_DESCRIPTION =
+  "Programming tutorials and self-help articles to make you a better developer and human being.";
+export const SITE_URL = "https://wisdomgeek.com";
+export const SITE_AUTHOR = "Saransh Kataria";
+export const SOCIAL_LINKS = {
+  twitter: "https://twitter.com/saranshk",
+  facebook: "https://www.facebook.com/wisdomgeek",
+  linkedin: "https://www.linkedin.com/in/saranshkataria",
+  youtube: "https://www.youtube.com/@wisdomgeek",
+  instagram: "https://www.instagram.com/kataria.saransh",
+};
